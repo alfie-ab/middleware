3faee4baf7d96939ff95429888b05a75faf3a73d
Alfie <alfie_ab@hotmail.co.uk>
2016-09-01 10:15:07 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+class HelloWorldApp
+  def self.call(env)
+    request = Rack::Request.new(env)
+    message = request.params['message'] || 'nothing!'
+
+    response = Rack::Response.new
+    response.write("Hello world! You said #{message}")
+    response.status = 202
+    response.finish
+  end
+end
+
+class Logger
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    p env.inspect
+    @app.call(env)
+  end
+end
